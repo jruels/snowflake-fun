5c3ec1c45f05046a893604ee811051eeb6ab1009
Jason Smith <jason@smithss.org>
2025-04-29 12:24:04 -0700

    reorg

--- experiments/ModernDataPipelines_ExperimentCleanup.sql
+++
@@ -1,8 +0,0 @@
--- Switch Context
-USE ROLE ACCOUNTADMIN;
-USE CITIBIKE_PIPELINES.PUBLIC;
-USE WAREHOUSE DATAPIPELINES_WH;
-
-call purge_files('trips_raw', '@streaming_data/');
-drop database if exists CITIBIKE_PIPELINES;
-drop warehouse if exists DATAPIPELINES_WH;
