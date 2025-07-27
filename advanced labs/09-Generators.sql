8a081345b792c77219d0782a7d03a279f1b19dfb
zhilkevich <97003121+zhilkevich@users.noreply.github.com>
2025-07-27 16:50:18 -0400

    Update 09-Generators.sql

--- advanced labs/09-Generators.sql
+++ advanced labs/09-Generators.sql
@@ -1,4 +1,4 @@
-/*----------------Snowflake Fundamentals 3-day class Lab:---------------------------
+/*----------------Snowflake Fundamentals 4-day class Lab:---------------------------
 1) Bonus Lab: 8 Range Generators Puzzles with multiple strategies 
 ----------------------------------------------------------------------------------*/
 
