cc7bb26c4845b8d8c0a74454d28e27682787f6ed
zhilkevich <97003121+zhilkevich@users.noreply.github.com>
2025-07-27 16:49:46 -0400

    Update 03-DataLoadLab.sql

--- advanced labs/03-DataLoadLab.sql
+++ advanced labs/03-DataLoadLab.sql
@@ -1,4 +1,4 @@
-/*----------------Snowflake Fundamentals 3-day class Lab:---------------------------
+/*----------------Snowflake Fundamentals 4-day class Lab:---------------------------
 1) Copy Into Command
 2) ON_ERROR attribute
 3) PATTERN attribute
